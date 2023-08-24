af8db2e331ec292ae3a1c702ff3e4f525a311885
rakhipatiil <124625893+rakhipatiil@users.noreply.github.com>
2023-08-24 20:29:17 +0530

    Add files via upload

---
+++ model_CNN.py
@@ -0,0 +1,49 @@
+
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD,RMSprop,Adam
+from keras.utils import np_utils
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import os
+import theano
+from PIL import Image
+from numpy import *
+# SKLEARN
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+#from Alexnet import AlexNet
+
+# input image dimensions
+img_rows, img_cols = 100, 100
+
+# number of channels
+img_channels = 3
+
+#%%
+#  data
+
+path1 = 'E:/Leukemia Detection/test_set/1'    #path of folder of images    
+path2 = 'E:/Leukemia Detection/test/1'  #path of folder to save images    
+
+listing = os.listdir(path1)
+num_samples=size(listing)
+print(num_samples)
+
+for file in listing:
+    im = Image.open(path1 + '\\' + file)  
+    img = im.resize((img_rows,img_cols))
+    gray = img.convert(mode='RGB')
+                #need to do some more processing here          
+    gray.save(path2 +'\\' +  file, "JPEG")
+
+imlist = os.listdir(path2)
+
+im1 = array(Image.open('input_data_resized/5/' + imlist[0])) # open one image to get size
+m,n = im1.shape[0:2] # get the size of the images
+imnbr = len(imlist) # get the number of images
+
